b4905e191e935482333aaffaa1cf656c456e4a0a
adiputrasinaga <sng.adiputra@gmail.com>
2019-08-09 09:11:13 +0700

    test

---
+++ test.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.6.5, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.6.5, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-single.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Fundnesia | Platform Investasi Pertanian</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/animatecss/animate.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+ 
+
+
+
+<section class="mbr-section content4 cid-qJTq1P9aew" id="content4-x">
+
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="title col-12 col-md-8">
+                <h2 class="align-center pb-3 mbr-fonts-style display-5">
+                    Bagaimana Cara kerja Fundnesia?</h2>
+                <h3 class="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-5"></h3>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="features1 cid-qJTqAMuCfn" id="features1-y">
+    
+    
+
+    
+    <div class="container">
+        <div class="media-container-row">
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbrib-touch-swipe"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-7"><strong>Pilih Jenis Investasi</strong></h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        Pelajari dan pilih setiap program yang ditawarkan sesuai dengan keingininan dan hasil yang ingin diterima.
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbrib-edit"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-7"><strong>Pantau Perkembangan</strong></h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        Informasi mengenai produk dan program yang dipilih akan diberitahukan secara berkala kepada Pemilik dana.
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbrib-cart-full"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-7"><strong>Penjualan Hasil Panen</strong></h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        Produk dari hasil panen akan dipastikan terserap oleh setiap rekanan dari Fundnesia pada saat waktu panen tiba.</p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbrib-like"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-7"><strong>
+                        Bagi Hasil</strong></h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                       Seluruh perhitungan pendapatan hasil panen akan dirincikan kedalam laporan yang dipertanggungjawabkan oleh Fundnesia.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<section class="timeline2 cid-qJTwvklUGp mbr-parallax-background" id="timeline2-16">
+
+    
+
+    
+
+    <div class="container align-center">
+        <h2 class="mbr-section-title pb-3 mbr-fonts-style display-2">Keuntungan Bersama Kami</h2>
+        <h3 class="mbr-section-subtitle pb-5 mbr-fonts-style display-5"></h3>
+
+        <div class="container timelines-container" mbri-timelines="">
+            <div class="row timeline-element reverse separline">      
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont mbri-cash" style="color: rgb(255, 255, 255);"></span>
+                </span>          
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                            Investasi Bermakna</h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">Anda telah memberikan kontribusi yang besar dan bermakna bagi para petani di Indonesia melalui pembiayaan di program ini.</p>
+                     </div>
+                </div>
+            </div>
+
+            <div class="row timeline-element  separline">
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont mbrib-user" style="color: rgb(255, 255, 255);"></span>
+                </span>
+                <div class="col-xs-12 col-md-6 align-left ">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">Pemberdayaan Petani</h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">Dengan investasi yang diberikan akan mengoptimalkan pendapatan petani dan fungsi lahan.</p>
+                    </div>
+                </div>
+            </div> 
+
+            <div class="row timeline-element reverse separline">
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont mbri-protect" style="color: rgb(255, 255, 255);"></span>
+                </span>
+                <div class="col-xs-12 col-md-6 align-left">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">Ketahanan pangan</h4>      
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">Setiap kontribusi dari investor turut andil dalam mewujudkan Ketahanan Pangan nasional.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row timeline-element ">
+                <span class="iconsBackground">
+                    <span class="mbr-iconfont mbri-like" style="color: rgb(255, 255, 255);"></span>
+                </span>
+                <div class="col-xs-12 col-md-6 align-left ">
+                    <div class="timeline-text-content">
+                        <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">Pengelolaan yang terpercaya</h4>
+                        <p class="mbr-timeline-text mbr-fonts-style display-7">Setiap proses dan pengawasan dilakukan secara berkala dan dilakukan oleh pakarnya.</p>
+                    </div>
+                </div>
+            </div>
+
+            
+
+            
+
+            
+
+            
+
+            
+
+            
+
+            
+
+            
+        </div>
+    </div>
+</section>
+
+<section class="clients cid-qJY1Td3fQ9" id="clients-1n">
+      
+
+    
+        <div class="container mb-5">
+            <div class="media-container-row">
+                <div class="col-12 align-center">
+                    
+                    <h3 class="mbr-section-subtitle mbr-light mbr-fonts-style display-5">Beberapa yang telah menjadi mitra kami</h3>
+                </div>
+            </div>
+        </div>
+
+    <div class="container">
+        <div class="carousel slide" data-ride="carousel" role="listbox">
+            <div class="carousel-inner" data-visible="4">
+                
+                
+                
+                
+                
+            <div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/logo-online-y777-800x251.jpg" class="img-responsive clients-img" alt="" title="">
+                            </div>
+                        </div>
+                    </div>
+                </div><div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/2.png" class="img-responsive clients-img">
+                            </div>
+                        </div>
+                    </div>
+                </div><div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/3.png" class="img-responsive clients-img">
+                            </div>
+                        </div>
+                    </div>
+                </div><div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/4.png" class="img-responsive clients-img">
+                            </div>
+                        </div>
+                    </div>
+                </div><div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/5.png" class="img-responsive clients-img">
+                            </div>
+                        </div>
+                    </div>
+                </div></div>
+            <div class="carousel-controls">
+                <a data-app-prevent-settings="" class="carousel-control carousel-control-prev" role="button" data-slide="prev">
+                    <span aria-hidden="true" class="mbri-left mbr-iconfont"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a data-app-prevent-settings="" class="carousel-control carousel-control-next" role="button" data-slide="next">
+                    <span aria-hidden="true" class="mbri-right mbr-iconfont"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="clients cid-qJTESWYYvX" id="clients-1f">
+      
+
+    
+        <div class="container mb-5">
+            <div class="media-container-row">
+                <div class="col-12 align-center">
+                    <h2 class="mbr-section-title pb-3 mbr-fonts-style display-2"></h2>
+                    <h3 class="mbr-section-subtitle mbr-light mbr-fonts-style display-5">Liputan Media</h3>
+                </div>
+            </div>
+        </div>
+
+    <div class="container">
+        <div class="carousel slide" data-ride="carousel" role="listbox">
+            <div class="carousel-inner" data-visible="4">
+                
+                
+                
+                
+                
+            <div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/1.png" class="img-responsive clients-img">
+                            </div>
+                        </div>
+                    </div>
+                </div><div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/2.png" class="img-responsive clients-img">
+                            </div>
+                        </div>
+                    </div>
+                </div><div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/3.png" class="img-responsive clients-img">
+                            </div>
+                        </div>
+                    </div>
+                </div><div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/4.png" class="img-responsive clients-img">
+                            </div>
+                        </div>
+                    </div>
+                </div><div class="carousel-item ">
+                    <div class="media-container-row">
+                        <div class="col-md-12">
+                            <div class="wrap-img ">
+                                <img src="assets/images/5.png" class="img-responsive clients-img">
+                            </div>
+                        </div>
+                    </div>
+                </div></div>
+            <div class="carousel-controls">
+                <a data-app-prevent-settings="" class="carousel-control carousel-control-prev" role="button" data-slide="prev">
+                    <span aria-hidden="true" class="mbri-left mbr-iconfont"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a data-app-prevent-settings="" class="carousel-control carousel-control-next" role="button" data-slide="next">
+                    <span aria-hidden="true" class="mbri-right mbr-iconfont"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/mbr-clients-slider/mbr-clients-slider.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/viewportchecker/jquery.viewportchecker.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/formoid/formoid.min.js"></script>
+  
+  
+ <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i></i></a></div>
+    <input name="animation" type="hidden">
+  </body>
+</html>
